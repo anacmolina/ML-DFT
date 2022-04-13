787dd8d30fcd68e16ca2849b43433fa872b634ef
marylou <marylou.gabrie@gmail.com>
2022-04-13 18:47:14 +0200

    test mixture script + Mixture class

---
+++ packages/flonaco-ml-dft/flonacomldft/mixture.py
@@ -0,0 +1,57 @@
+from torch._C import device
+import numpy as np
+import torch
+import torch.nn as nn
+# import torch.nn.functional as F
+from flonacomldft.real_nvp_mlp import RealNVP_MLP
+# from  torch.distributions.uniform import Uniform
+
+
+class Mixture(nn.Module):
+    def __init__(self, models, init_weights=None, device='cpu'):
+        super(Mixture, self).__init__()
+
+        self.device = device
+        self.dim = models[0].dim
+
+        self.models = models
+
+        if init_weights is None:
+            self.weights = torch.ones((len(models),), device=device) / len(models)
+        else:
+            self.weights = torch.tensor(init_weights).to(device)
+
+        self.weights.requires_grad_()
+
+    def sample(self, n, shuffle=True, return_mus=False):
+
+        a = torch.multinomial(self.weights, n, replacement=True)
+        mus, counts = a.unique(return_counts=True)   
+
+        xs = []
+        cs = []
+        for mu, count in zip(mus, counts):
+            xs.append(self.models[mu].sample(count))
+            cs += [mu.item()] * count
+
+        x = torch.cat(xs)
+
+        if shuffle:
+            perm = torch.randperm(n)
+            x = x[perm, :]
+            cs = torch.tensor(cs)[perm]
+
+        if return_mus:
+            return x, cs
+        else:
+            return x
+
+    def nll(self, x):
+        args = []
+        for model, weight in zip(self.models, self.weights):
+            args.append(- model.nll(x) + torch.log(weight))
+
+        return - torch.logsumexp(torch.stack(args), 0)
+
+    def U(self, x):
+        return self.nll(x)
