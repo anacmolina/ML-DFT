decb3174c5443a69b78ace6d8d54835bedfa38fa
anacmolina <anacmolina@udea.edu.co>
2022-07-12 11:13:11 -0400

    update iters 10000

--- packages/flonaco-ml-dft/flonacomldft/md_mcmc.py
+++ packages/flonaco-ml-dft/flonacomldft/md_mcmc.py
@@ -90,7 +90,7 @@ def run_nf(zmat, model):
 
     _ = train(model, 
            x_tensor,
-           n_iter=1000,
+           n_iter=10000,
            lr=1e-3,
            bs=100,
            use_scheduler=False,
