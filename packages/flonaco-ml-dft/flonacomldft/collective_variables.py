c852240ca66d10dd2d274888d8cbd283f009bebe
anacmolina <anac.molina@udea.edu.co>
2022-09-08 13:59:27 -0500

    cleaning collective variables file

--- packages/flonaco-ml-dft/flonacomldft/collective_variables.py
+++ packages/flonaco-ml-dft/flonacomldft/collective_variables.py
@@ -6,32 +6,46 @@ d=2.8
 rij_d = lambda rij: rij/d
 
 def X_i(i, r):
+   
    value=0
+   
    for j in range(len(r)):
+   
       if(i!=j):
+   
          value = value + (1 - rij_d(r[i][j])**8)/(1 - rij_d(r[i][j])**16)
+   
    return value
 
 # Coordination number  
 def C(atoms):
+   
    r = atoms.get_all_distances()
+   
    return np.array([X_i(i, r) for i in range(atoms.get_global_number_of_atoms())]).sum()
 
 # Radius of gyration
 def R(atoms):
+   
    r_rcm = atoms.get_positions() - atoms.get_center_of_mass()
    result = np.sqrt(np.array([np.linalg.norm(ri)**2 for ri in r_rcm]).sum()/atoms.get_global_number_of_atoms())
+   
    return result 
 
-   # Getting the collective variables from internal coordinates
+# Getting the collective variables from internal coordinates
 def get_CVs(data):
-   #symbols = np.full(6, 'Ag')
+   
    C_vals = []
    R_vals = []
+   
    for x in data:
+   
       ag6 = Structure()
       ag6.build_zmat_matrix(x)
+   
       atoms = ag6.molecule                                                                                  
+   
       C_vals.append(C(atoms))
       R_vals.append(R(atoms))
+   
    return C_vals, R_vals
