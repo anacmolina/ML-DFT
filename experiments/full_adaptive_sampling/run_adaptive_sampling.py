9b3d98b33c84d3cfe8c7c5a264e82b95468fde81
anacmolina <anac.molina@udea.edu.co>
2022-10-05 17:50:28 -0500

    full_adaptative

--- experiments/full_adaptive_sampling/run_adaptive_sampling.py
+++ experiments/full_adaptive_sampling/run_adaptive_sampling.py
@@ -1,35 +1,16 @@
-import pickle
+from itertools import count
+from ase.parallel import parprint as print
 
 import torch
 import numpy as np
+import gpaw.mpi as mpi
 
-from ase.io.trajectory import Trajectory
+from flonacomldft.data_utils import get_path, load_zmat_csv, load_from_pickle
 
 from flonacomldft.mixture import Mixture
 from flonacomldft.sampling import run_metropolis
-
-from flonacomldft.data_utils import (
-    get_path,
-    load_zmat_csv,
-    save_pickle_file,
-    load_from_pickle
-    )
-
-from flonacomldft.dft_utils import (
-    get_internal_coordinates,
-    Structure
-)
-
-from flonacomldft.md_mcmc import (
-    init_model,
-    get_models,
-    get_mix_data,
-    get_pos_energy,
-    train_nf,
-    run_md_get_zmat
-)
-
-import gpaw.mpi as mpi
+from flonacomldft.internal_coordinates import get_mix_data
+from flonacomldft.md_mcmc import get_models
 
 # Seed initialization for random generations
 ranks = np.arange(0, mpi.world.size)
@@ -40,153 +21,137 @@ num_seed = np.array([0])
 
 if rank == 0:
     num_seed = np.array([np.random.randint(1e5)])
-    
+
 comm.broadcast(num_seed, 0)
 
-print("Rank: %d \t Seed: %d"%(rank, num_seed[0]))
-torch.manual_seed(num_seed[0])
+print("Rank: %d \t Seed: %d" % (rank, num_seed[0]))
+# torch.manual_seed(num_seed[0])
+torch.manual_seed(36)
 
-mpi.world.barrier()
+# Run MD for both isomers
 
-# Loading previous trajectories
-path = get_path()
-traj_is1 = Trajectory(path+'ag6_is1_lcao.traj')
-traj_is2 = Trajectory(path+'ag6_is2_lcao.traj')
+# loading traj in internal coordinates
+data_is1 = load_zmat_csv("is1")
+data_is2 = load_zmat_csv("is2")
 
-# Getting internal coordinates of previous trajectories info
-"""
-From .traj files
+xis, uis, cis = get_mix_data(data_is1, data_is2)
 
-data_is1 = get_internal_coordinates(traj_is1)
-data_is2 = get_internal_coordinates(traj_is2) 
-"""
+# Pretrain mlps and flows
 
-# From csv files previously generated
-data_is1 = load_zmat_csv('is1')
-data_is2 = load_zmat_csv('is2')
+# loading pretrain mlps models
+init_mlp_is1 = load_from_pickle(get_path() + "mlp_is1")
+init_mlp_is2 = load_from_pickle(get_path() + "mlp_is2")
 
-# Train initial NFs
-#init_nf_is1 = train_nf(data_is1, init_model(data_is1))
-#init_nf_is2 = train_nf(data_is2, init_model(data_is2))
-init_nf_is1 = load_from_pickle(get_path() + 'training_is1')
-init_nf_is2 = load_from_pickle(get_path() + 'training_is2')
-""" 
-List to save info:
+# loading pretrain flows models
+init_nf_is1 = load_from_pickle(get_path() + "training_is1")
+init_nf_is2 = load_from_pickle(get_path() + "training_is2")
 
-    - NFs trained
-    - Models
-"""
-nf = [[init_nf_is1, init_nf_is2], ]
-models = [get_models(nf[0]), ]
+# Adaptative MCMC
 
-# Shuffle data
-xis, uis, cis =  get_mix_data(data_is1, data_is2)
+flow_train = [
+    [init_nf_is1, init_nf_is2],
+]
+flow = [
+    get_models(flow_train[0]),
+]
+mlps = [[init_mlp_is1, init_mlp_is2]]
 
-"""
-MCMC parameters:
-    - n_runs: MCMC runs 
-    - n_chains: MCMC chains
-    - n_sts: MCMC steps
-"""
+weights = torch.tensor([0.5, 0.5]).detach()
+mixture = Mixture(flow[0], weights)
 
-n_runs = 10
-n_chains = 1
+n_runs = 1
+n_chains = 5
 n_sts = 3
+md_iters = 100
+energy_type = "dft"#"mlp-dft"
 
-# MD iterations
-md_iters = 10
+# init sampling
+_ = run_metropolis(
+    model=mixture,
+    u_init=uis[:n_chains],
+    x_init=xis[:n_chains, :],
+    count_init=cis[:n_chains],
+    n_chains=n_chains,
+    n_steps=n_sts,
+    energy_type=energy_type,
+    mlps=mlps[0],
+    mixture=True,
+)
+""" print(_['xs'])
+print(_['accs'])
+print(_['ind_dft']) """
 
-# Building the NF mixture model
-mixture = Mixture(models[0], torch.tensor([0.75, 0.25]).detach())
+xs_acc = [
+    _["xs"],
+]
+us_acc = [
+    _["us"],
+]
+accs_s = [
+    _["accs"],
+]
+cs_acc = [
+    _["counts"],
+]
 
-# Running the initial MCMC
-xs_, accs_, us_, counts_ = run_metropolis(model=mixture, u_init=uis, x_init=xis, count_init=cis, 
-                                            n_sample=n_chains, n_steps=n_sts, mixture=True)
 
-"""
-MCMC Results:
-    - xs_acc: Configs mcmc 
-    - us_acc: Energies mcmc
-    - accs_s: Acceptance information
-    - cs_acc: (State (0: Planar, 1: 3D)) Isomer configuration
-"""
+def T(x):
+    return x.permute(*torch.arange(x.ndim - 1, -1, -1))
 
-xs_acc = [xs_, ]
-us_acc = [us_, ]
-accs_s = [accs_, ] 
-cs_acc = [counts_, ]
 
-# MCMC + MD
+USE_DFT_ENERGIES = False
+if energy_type == "dft" or energy_type == "mlp-dft":
+    USE_DFT_ENERGIES = True
 
-for i in range(n_runs):
-    data_acc = torch.cat((xs_acc[i].T, us_acc[i].reshape(n_sts,n_chains,1).T), dim=0).T
 
-    data_ex = data_acc.reshape(n_sts*n_chains, 13)
-    
-    cs_ex = cs_acc[i].reshape(n_sts*n_chains, 1) 
-    n_is2 = cs_ex.sum(dim=0)
-    cs_ex = torch.ones(data_ex.shape)*cs_ex
+# retrain flows
+i=0
 
-    select = torch.ones(data_acc.shape)*cs_acc[i].reshape(n_sts,n_chains,1)
-    
-    is1_acc = data_acc[~select.bool()].reshape(n_sts*n_chains-n_is2.int(),13).unique(dim=0)
-    is2_acc = data_acc[select.bool()].reshape(*n_is2.int(),13).unique(dim=0)
-    
-    if is1_acc.nelement!=0:
-        data_is1 = torch.cat((data_is1, is1_acc))
-    elif is2_acc.nelement!=0:
-        data_is2 = torch.cat((data_is2, is2_acc))
-    else:
-        pass
+data_for_flows = T(
+    torch.cat(
+        (
+            T(xs_acc[i]),
+            T(cs_acc[i].reshape(n_sts, n_chains, 1)),
+        ),
+        dim=0,
+    )
+)
 
-    x_ = None
-    c_ = cs_acc[i].unique_consecutive(dim=0)[-1]
-    
-    if c_==0:
-        name='is1'
-        x_ = data_is1[-1, :-1]
-    elif c_==1:
-        name='is2'
-        x_ = data_is1[-1, :-1]
-    else:
-        raise RuntimeError('Unknown value')
-     
-    ag6 = Structure()
-    ag6.build_zmat_matrix(x_)
-   
-    mpi.world.barrier()
-    if ((i+1)%2==0):
-        md_data = run_md_get_zmat(ag6.molecule, md_iters, name+'_'+str(i+1), starting=False)
-    
-        if c_==0:
-            data_is1 = torch.cat((data_is1, md_data))
-        elif c_==1:
-            data_is2 = torch.cat((data_is2, md_data))                             
-        else:
-            pass
-    
+data_for_flows = data_for_flows.reshape(n_sts * n_chains, data_for_flows.shape[-1])
+data_for_flows = data_for_flows.unique(dim=0)
 
-    nf_is1 = train_nf(data_is1, models[i][0])
-    nf_is2 = train_nf(data_is2, models[i][1])
+sel = data_for_flows[:, -1]==1
+is1_prop = data_for_flows[~sel]
+is2_prop = data_for_flows[sel]
 
-    nf.append([nf_is1, nf_is2])
-                             
-    xis, uis, cis =  get_mix_data(data_is1, data_is2)
-    model_is1, model_is2 = get_models(nf[i+1])
-    models.append([model_is1, model_is2])
+#print(data_for_flows)
+#del data_for_flows
 
-    mixture = Mixture(models[i+1], torch.tensor([0.75, 0.25]).detach())
-    xs__, accs__, us__, counts__ = run_metropolis(model=mixture, u_init=uis, x_init=xis, count_init=cis, n_sample=n_chains, n_steps=n_sts, mixture=True)
+print(is1_prop)
+print(is2_prop)
 
-    xs_acc.append(xs__)
-    accs_s.append(accs__)
-    us_acc.append(us__)                    
-    cs_acc.append(counts__)
 
-    filename_ = 'mcmc_md_'+str(i)+'_'+str(n_chains)+'_'+str(n_sts)
-    save_pickle_file([xs_acc, us_acc, accs_s, cs_acc], filename_)
 
-mcmc_md = [data_is1, data_is2, xs_acc, accs_s, us_acc, cs_acc]
+# retrain MLPs
+if USE_DFT_ENERGIES:
 
-filename = 'mcmc_md_complete_'+str(n_runs)+'_'+str(n_chains)+'_'+str(n_sts)+''
-save_pickle_file(mcmc_md, filename)
+    data_for_mlp = T(
+        torch.cat(
+            (
+                T(xs_acc[i]),
+                T(us_acc[i].reshape(n_sts, n_chains, 1)),
+                T(cs_acc[i].reshape(n_sts, n_chains, 1)),
+            ),
+            dim=0,
+        )
+    )
+
+    #print(data_for_mlp)
+
+    if energy_type == "dft":
+        # use all
+        data_ex = data_for_mlp.reshape(n_sts * n_chains, data_for_mlp.shape[-1])
+
+    elif energy_type == "mlp-dft":
+        # use only ind_dft
+        print('hi :(, T_T')#_["ind_dft"])
