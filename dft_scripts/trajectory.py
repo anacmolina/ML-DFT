ec585ae9ccf11301b6b9242480c8636b92bdd61f
anacmolina <anacmolina@udea.edu.co>
2022-10-27 10:23:29 -0500

    changes

--- dft_scripts/trajectory.py
+++ dft_scripts/trajectory.py
@@ -1,38 +1,50 @@
 import os
-import sys
 import numpy as np
 
+from flonacomldft.molecule import Ag6Isomers
+
 from ase.parallel import parprint
-from ase.io import read
 from gpaw import GPAW
 from ase.optimize import BFGS
-from  ase.md.nvtberendsen import NVTBerendsen
+from ase.md.nvtberendsen import NVTBerendsen
 from ase import units
-from ase.md.velocitydistribution import (MaxwellBoltzmannDistribution,
-                                         Stationary, ZeroRotation)
+from ase.md.velocitydistribution import (
+    MaxwellBoltzmannDistribution,
+    Stationary,
+    ZeroRotation,
+)
 
-if os.path.isdir('/mnt/home/amolina/ceph/database'):
-   ceph_home = '/mnt/home/amolina/ceph/database'
-elif os.path.isdir('/Users/marylou/Dropbox/Prof/Experiments/_ceph/ml-dft/'):
-   ceph_home = '/Users/marylou/Dropbox/Prof/Experiments/_ceph/ml-dft/'
-elif os.path.isdir('/home/anacristina/ml_dft_project/database/'):
-    ceph_home = '/home/anacristina/ml_dft_project/database/'
-elif os.path.isdir('/home/amolina/ml_dft_project/database/'):
-    ceph_home = '/home/amolina/ml_dft_project/database/'
+if os.path.isdir("/mnt/home/amolina/ceph/database"):
+    ceph_home = "/mnt/home/amolina/ceph/database"
+elif os.path.isdir("/Users/marylou/Dropbox/Prof/Experiments/_ceph/ml-dft/"):
+    ceph_home = "/Users/marylou/Dropbox/Prof/Experiments/_ceph/ml-dft/"
+elif os.path.isdir("/home/anacristina/ml_dft_project/database/"):
+    ceph_home = "/home/anacristina/ml_dft_project/database/"
+elif os.path.isdir("/home/amolina/ml_dft_project/database/"):
+    ceph_home = "/home/amolina/ml_dft_project/database/"
 else:
-   raise RuntimeError('Data path not understood')
+    raise RuntimeError("Data path not understood")
 
-isomer = str(sys.argv[1])
-mode = str(sys.argv[2])
-name = isomer+"_"+mode
-mol = read(ceph_home + 'ag6_'+name+'.xyz')
+isomer = "ag6_planar"
+mode = "lcao"
+name = isomer + "_" + mode
+
+mol = Ag6Isomers(isomer)
 
 mol.set_cell([16, 16, 16])
 mol.set_pbc(True)
 mol.center()
 
-calc = GPAW(mode="lcao", h=0.2, basis="pvalence.dz", spinpol=True, xc="PBE", symmetry="off", nbands = -4, txt='ag6\
-_'+name+'.out')
+calc = GPAW(
+    mode=mode,
+    h=0.2,
+    basis="pvalence.dz",
+    spinpol=True,
+    xc="PBE",
+    symmetry="off",
+    nbands=-4,
+    txt=name + ".out",
+)
 
 mol.set_calculator(calc)
 
@@ -40,7 +52,7 @@ MaxwellBoltzmannDistribution(mol, temperature_K=300)
 Stationary(mol)
 ZeroRotation(mol)
 
-dyn = NVTBerendsen(mol, 5 * units.fs, taut = 50, temperature_K=300, trajectory='ag6_'+name+'_md.traj')
+dyn = NVTBerendsen(
+    mol, 5 * units.fs, taut=50, temperature_K=300, trajectory=name + "_md.traj"
+)
 dyn.run(5000)
-
-
