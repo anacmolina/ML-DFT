bug fixed - nll calculation

experiments/premodeling/train_flow_model.py
@@ -18,7 +18,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # for flows
 
 
-n_iter = 5000 #100 # long_training 10000
+n_iter = 500 #100 # long_training 10000
 lr = 1e-4
 mode_label = 1 # or 2
 
@@ -59,6 +59,6 @@ out = train_flow(
     grad_clip=1e4,
 )
 
-f = "is{:d}_flow_dic_training_long_training.pkl".format(mode_label)
+f = "is{:d}_flow_dic_training_test.pkl".format(mode_label)
 save_pickle_file(out, f)
 

packages/flonaco-ml-dft/flonacomldft/sampling.py
@@ -92,8 +92,8 @@ def run_metropolis(
         x_new = x_new.clone().detach().float()
         isomer_new = isomer_new.clone().detach().float()
 
-        nll_x = model.nll(x_new)
-        nll_x_init = model.nll(x_init)
+        nll_x = model.nll(model.rad_center(x_new, isomer_new))
+        nll_x_init = model.nll(model.rad_center(x_init, isomer_init))
        
         if "mlp" in energy_type:
             u_new = torch.zeros((n_chains, 1))
@@ -150,6 +150,7 @@ def run_metropolis(
         ratio += beta * u_init - nll_x_init
         ratio = torch.exp(ratio)
         u = torch.rand_like(ratio)
+
         acc = u < torch.min(ratio, torch.ones_like(ratio))
 
         if use_dft:
@@ -167,6 +168,7 @@ def run_metropolis(
         else:
             isomer_new = isomer_init
 
+        print('accs: {:2f}'.format(acc.float().mean()))
         
         xs.append(x_new.float().clone())
         us.append(u_new.float().clone())

tests/test_metropolis_mixture.py
@@ -32,8 +32,8 @@ torch.manual_seed(num_seed[0])
 
 # mcmc chains parameters
 
-n_chains = 50
-n_steps = 100
+n_chains = 100
+n_steps = 150
 energy_type = 'mlp'
 
 # load data
@@ -65,6 +65,11 @@ flow_models = np.array([flow_is1['model'],
 
 mixture = Mixture(flow_models, torch.tensor([0.5, 0.5]).detach())
 
+#xs, cs =  mixture.sample(5, return_mus=True)
+#print(xs, cs)
+#xs_rad = mixture.rad_center(xs.clone(), cs.bool())
+#print(mixture.nll(xs_rad), mixture.nll(xs))
+
 # mlp models
 
 mlp_is1 = load_pickle_file('is1_mlp_dic_training.pkl')
@@ -88,7 +93,7 @@ out = run_metropolis(
     mlp_models=mlp_models,
     mixture=True,
     T=300,
-    with_tqdm=True,
+    with_tqdm=False,
 )
 
 f = "experiments/mcmc_mixture_chains_{:d}_steps_{:d}.pkl".format(n_chains, n_steps)