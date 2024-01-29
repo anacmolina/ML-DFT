c014a0e4bc802e736f6f29dbc0f692f3d3ad049f
anacmolina <anac.molina@udea.edu.co>
2024-01-29 10:38:17 -0500

    optical spectra files

---
+++ experiments/get_optical_spectra.py
@@ -0,0 +1,61 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+from flonacomldft.optical_spectra import compute_optical_spectra
+import os
+import torch
+import numpy as np
+from ase.io import read
+#from flonacomldft.utils.silver_isomers_utils import get_molecule_isomer_minima
+import argparse
+from flonacomldft.internal_coordinates import Coordinates_mapping
+from flonacomldft.utils.io_utils import load_pickle_file, get_project_path
+import gpaw.mpi as mpi
+
+ranks = np.arange(0, mpi.world.size)
+rank = mpi.rank
+
+parser = argparse.ArgumentParser(description='Compute optical spectra for a molecule.')
+parser.add_argument('-isomer', '--isomer', type=int, help='isomer to compute optical spectra for')
+parser.add_argument('-path', '--path', type=str, default=os.getcwd(), help='path to write results to')
+parser.add_argument('-aid', '--adaptive-id', type=int, help='adaptive sampling id')
+parser.add_argument('-range', '--range', type=int, nargs='+', default=0, help='range of chains to compute spectra for')
+parser.add_argument('-etype', '--energy-type', type=str, default='dft', help='Energy type used in the adaptive')
+parser.add_argument('-row', '--row', type=int, default=-1, help='Number of rows in the plot')
+
+args = parser.parse_args()
+
+#molecule = get_molecule_isomer_minima('is{:d}'.format(args.isomer))
+
+adaptive_path = {'dft': '1-adaptive/results_adaptive_is'+str(args.isomer)+'_'+str(args.adaptive_id)+'/adaptive_sampling_is'+str(args.isomer)+'_'+str(args.adaptive_id)+'.pkl',
+                    'mlp': '2-adaptive-mlp/results_adaptive_is'+str(args.isomer)+'_'+str(args.adaptive_id)+'/adaptive_sampling_is'+str(args.isomer)+'_'+str(args.adaptive_id)+'.pkl'}
+
+xs = torch.cat(load_pickle_file(adaptive_path[args.energy_type], path=get_project_path())['xs'])[args.row]
+
+coord_mapping = Coordinates_mapping()
+
+for i in [8, 15]:#range(args.range[0], args.range[1]):
+    print('Computing Sample {:d}'.format(i))
+    x = xs[i].reshape(1, -1)
+    molecule = coord_mapping.build_molecule_from_real_centered(x, isomer=args.isomer)[0]
+    molecule.center(vacuum=8)
+
+    if rank == 0:
+
+        if not os.path.exists(args.path+'/sample_'+str(i)):
+            os.makedirs(args.path+'/sample_'+str(i))
+
+        from ase.visualize.plot import plot_atoms
+        import matplotlib.pyplot as plt
+
+        plt.figure()
+        plot_atoms(molecule)
+        plt.savefig(args.path+'/sample_'+str(i)+'/molecule.png')
+
+    mpi.world.barrier()
+
+    try:
+        compute_optical_spectra(molecule, args.path+'/sample_'+str(i))
+        print('Success!')
+    except:
+        print('Failed!')
---
+++ packages/flonaco-ml-dft/flonacomldft/optical_spectra.py
@@ -0,0 +1,129 @@
+from gpaw import GPAW, Mixer
+from gpaw.mpi import world
+from gpaw import GPAW
+from gpaw.lrtddft2 import LrTDDFT2
+from gpaw.lrtddft2.lr_communicators import LrCommunicators
+from ase.parallel import paropen
+
+
+def write_ground_state(atoms, path, calc=None):
+
+    if calc is None:
+        calc = GPAW(h=0.2,
+                nbands=-30,
+                xc='PBE',
+                #poissonsolver={'remove_moment': 1 + 3 + 5},
+                poissonsolver={'name': 'MomentCorrectionPoissonSolver',
+                               'poissonsolver': 'fast',
+                               'moment_corrections': 1 + 3 + 5},
+                convergence={'bands': -8},
+                txt=path+'/gs.out',
+                mixer=Mixer(beta=0.1),
+	        eigensolver = "cg")
+
+    atoms.calc = calc
+    atoms.get_potential_energy()
+    calc.write(path+'/gs.gpw')
+
+def unoccupied_states(path):
+
+    calc = GPAW(path+'/gs.gpw')
+    
+    calc = calc.fixed_density(nbands=-30,
+                          convergence={'bands': "all"},
+                          maxiter=1000,
+                          txt=path+'/unocc.out',
+                          symmetry="off"
+                          )
+    
+    calc.write(path+'/unocc.gpw', mode='all')
+
+
+def linear_response(path):
+
+    max_energy_diff = 5.5 #eV
+    w=0.08
+
+    dd_size = 2 * 2 * 2
+    eh_size = world.size // dd_size
+    assert eh_size * dd_size == world.size
+    lr_comms = LrCommunicators(world, dd_size, eh_size)
+
+    calc = GPAW(path+'/unocc.gpw',
+            communicator=lr_comms.dd_comm)
+    
+    lr = LrTDDFT2(path+'/lr2', calc,
+              fxc='LDA',
+              max_energy_diff=max_energy_diff,
+              recalculate=None,  # Change this to force recalculation
+              lr_communicators=lr_comms,
+              txt=path+'/lr2_with_%05.2feV.out' % max_energy_diff)
+
+    # This is the expensive part triggering the calculation!
+    lr.calculate()
+
+    spec = lr.get_spectrum(path+'/spectrum_with_%05.2feV.dat'
+                       % max_energy_diff,
+                       min_energy=0,
+                       max_energy=10,
+                       energy_step=0.01,
+                       width=w)
+
+    # Get and write transitions
+    trans = lr.get_transitions(path+'/transitions_with_%05.2feV.dat'
+                           % max_energy_diff,
+                           min_energy=0.0,
+                           max_energy=10.0)
+
+def lr_get_transitions(path):
+
+    max_energy_diff = 5.5  # eV
+
+    calc = GPAW(path+'/unocc.gpw')
+    lr = LrTDDFT2(path+'/lr2', calc,
+                  fxc='LDA',
+                  max_energy_diff=max_energy_diff,
+                  txt='-')
+    
+    spec = lr.get_spectrum(#'spectrum_with_%05.2feV.dat'
+                           #% max_energy_diff,
+                           min_energy=0,
+                           max_energy=10,
+                           energy_step=0.01,
+                           width=0.1)
+
+    # Get and write transitions
+    trans = lr.get_transitions(#'transitions_with_%05.2feV.dat'
+                               #% max_energy_diff,
+                               min_energy=0.0,
+                               max_energy=10.0)
+
+    # Get and write transition contributions
+    indexlist = [i for i in range(0,20)]
+    for index in indexlist:
+        f2 = lr.get_transition_contributions(index_of_transition=index)
+        with paropen(path+'/tc_%03d_with_%05.2feV.txt'
+                 % (index, max_energy_diff), 'w') as f:
+            f.write('Transition %d at %.2f eV\n' % (index, trans[0][index]))
+            f.write(' %5s => %5s  contribution\n' % ('occ', 'unocc'))
+            for (ip, val) in enumerate(f2):
+                if (val > 1e-3):
+                    f.write(' %5d => %5d  %8.4f%%\n' %
+                        (lr.ks_singles.kss_list[ip].occ_ind,
+                         lr.ks_singles.kss_list[ip].unocc_ind,
+                         val / 2. * 100))
+
+def compute_optical_spectra(atoms, path):
+
+    write_ground_state(atoms, path)
+    unoccupied_states(path)
+    linear_response(path)
+    lr_get_transitions(path)
+
+
+    
+
+
+
+
+    
