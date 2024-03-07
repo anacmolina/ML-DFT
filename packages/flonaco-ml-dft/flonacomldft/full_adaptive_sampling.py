9d74994e0b8f87cd24f2444acf2e07f241755430
anacmolina <anac.molina@udea.edu.co>
2024-03-07 13:50:37 -0500

    add iteration increasing

--- packages/flonaco-ml-dft/flonacomldft/full_adaptive_sampling.py
+++ packages/flonaco-ml-dft/flonacomldft/full_adaptive_sampling.py
@@ -335,6 +335,8 @@ def run_adaptive_sampling(
 
                     dict_new_mlps.append(dict_new_mlp)
 
+                    mlp_hyperparams[mode]['n_iter'] = mlp_hyperparams[mode]['n_iter'] + 3000
+
         dict_flows.append(dict_new_flows)
 
         if train_mlp_models and use_calc:
